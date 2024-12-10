0366a101323d52a2d837b3adedd7a520a3506511
ana <ana@DESKTOP-GFQ6MPI>
2024-12-10 18:50:07 +0200

    Add project files.

---
+++ SocialMediaAppAna/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using SocialMediaAppAna.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialMediaAppAna.Data
+{
+    // PASUL 3: useri si roluri
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        
+        //aici se pun restul claselor
+        //fct pt many-to-many
+        
+    }
+}
